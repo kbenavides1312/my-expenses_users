f8aeb76381302bd005b34fe71e8d639304567d90
Kenneth Benavides <kbr1312@gmail.com>
2024-04-12 23:05:09 -0600

    feat: provide token for login

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,8 @@ module main
 go 1.16
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.22.0
--- src/main.go
+++ src/main.go
@@ -5,12 +5,17 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+	"fmt"
 	"os"
 
 	"github.com/gorilla/mux"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v5"
 	_ "github.com/lib/pq"
 )
+
+var secretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
 
 type User struct {
 	ID    int    `json:"id"`
@@ -21,6 +26,11 @@ type User struct {
 type UserPayload struct {
 	User
 	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	User `json:"user"`
+	Token string `json:"token"`
 }
 
 type UserDB struct {
@@ -37,6 +47,37 @@ func CheckPasswordHash(password, hash string) bool {
     err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
     return err == nil
 }
+
+func createToken(email string) (string, error) {
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, 
+        jwt.MapClaims{ 
+        "email": email, 
+        "exp": time.Now().Add(time.Minute * 10).Unix(), 
+        })
+
+    tokenString, err := token.SignedString(secretKey)
+    if err != nil {
+    	return "", err
+    }
+
+ return tokenString, nil
+}
+
+func verifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	   return secretKey, nil
+	})
+   
+	if err != nil {
+	   return err
+	}
+   
+	if !token.Valid {
+	   return fmt.Errorf("invalid token")
+	}
+   
+	return nil
+ }
 
 func main() {
 	//connect to database
@@ -64,7 +105,21 @@ func main() {
 
 	//start server
 	log.Print("listening...")
-	log.Fatal(http.ListenAndServe(":8000", jsonContentTypeMiddleware(router)))
+	log.Fatal(http.ListenAndServe(":8000", corsMiddleware(jsonContentTypeMiddleware(router))))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ALLOWED_ORIGINS"))
+		if r.Method == "OPTIONS" {
+			log.Println("preflight", r.Method)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, Accept")
+			w.Header().Set("Content-Type", "application/json")
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func jsonContentTypeMiddleware(next http.Handler) http.Handler {
@@ -205,9 +260,14 @@ func logIn(db *sql.DB) http.HandlerFunc {
 			return
 		}
 
-		
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		json.NewEncoder(w).Encode(u.User)
+		tokenString, err := createToken(up.Email)
+		if err != nil {
+			fmt.Errorf("user %v unauthorized; error: %v", up.Email, err)
+			w.WriteHeader(http.StatusUnauthorized)
+		}		
+		resp := LoginResponse{User: up.User, Token: tokenString}
+		fmt.Printf("user %v authorized\n", up.Email)
+		json.NewEncoder(w).Encode(resp)
+		return
 	}
 }
